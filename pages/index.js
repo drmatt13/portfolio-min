3498c10dde3b82a5b341020975359a15d438142c
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2022-07-23 01:08:21 -0400

    7/23

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import Websites from "../components/Websites";
 export default function Home() {
   return (
     <>
-      <div className="min-h-[500px] xs:min-h-[525px] md:min-h-[575px] xl:min-h-[600px] h-full h-fill flex flex-col">
+      <div className="min-h-[500px] xs:min-h-[525px] md:min-h-[575px] xl:min-h-[600px] h-full flex flex-col">
         <div className="websites flex-1 flex flex-col justify-center">
           <Websites />
         </div>
-        <div className="absolute bottom-0 flex justify-center overflow-hidden animate-fade-in">
+        <div className="flex justify-center overflow-hidden animate-fade-in">
           <MirrorIcons />
         </div>
       </div>
